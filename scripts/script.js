2ac79acedc335a97d2e873f8abfbc1fd6f3052aa
Raian-Siddique <raian0197@gmail.com>
2020-07-24 04:23:44 +0600

    second page structure done

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,16 @@
+//login button event handler
+
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function () {
     const loginArea = document.getElementById("login-area");
     loginArea.style.display = "none";
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display = "block";
+})
+
+
+//deposit button event handler      
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function () {
+    console.log("deposit click");
 })
