7c3027a22016d0aee4f7c486b3351a136c2765aa
Raian-Siddique <raian0197@gmail.com>
2020-07-24 11:52:02 +0600

    commenting addition comment

--- scripts/script.js
+++ scripts/script.js
@@ -26,51 +26,56 @@
 // })
 
 
-
-
-
 // login button event handler
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function () {
     const loginArea = document.getElementById("login-area");
     loginArea.style.display = "none";
     const transactionArea = document.getElementById("transaction-area");
-    transactionArea.style.display = "block";
+    transactionArea.style.display = "block"; // we display the initial hide component from the css .if click then unhide from  
 })
 
-//deposit button event handler
+
+
+
+//8888888888888888        deposit button event handler        888888888888888888888
+
 const depositBtn = document.getElementById("addDeposit");
 depositBtn.addEventListener("click", function () {
     const depositNumber = getInputNumber("depositAmount");
-
-
-    // const previousDepositAmount = document.getElementById("depositAmount").Value;
-    // const depositNumber = parseFloat(previousDepositAmount);
-
-    // const currentDeposit = document.getElementById("currentDeposit").innerText;
-    // const currentDepositNumber = parseFloat(currentDeposit);
-    // const totalDeposit = depositNumber + currentDepositNumber;
-    // document.getElementById("currentDeposit").innerText = totalDeposit;
+    //getInputNumber() is a function to take the new input from the user.
+    //and return the num value, to further add or subtract it with the old value.
 
     updateSpanText("currentDeposit", depositNumber);
+    //with updateSpanText() we ate calling the function and  passing an id in the function
 
     updateSpanText("currentBalance", depositNumber);
+    // in terms of balance passing currentBalance, for Deposit Passing currentDeposit
 
-    document.getElementById("depositAmount").value = "";
+    document.getElementById("depositAmount").value = ""; // just reloading the empty string
 })
-//Withdraw button event handler
+
+//88888888888888888888888     Withdraw button event handler    8888888888888888888888888888
+
 const withdrawBtn = document.getElementById("addWithdraw");
 withdrawBtn.addEventListener("click", function () {
     const withdrawNumber = getInputNumber("withdrawAmount");
-    console.log(withdrawNumber);
+    updateSpanText("currentWithdraw", withdrawNumber);
+    updateSpanText("currentBalance", -1 * withdrawNumber); //it will subtract if we withdraw amount.
+    document.getElementById("withdrawAmount").value = "" //it will reload auto after we put a value.thats why put an empty string
 
 })
 
+//888888888888888888888    Function section   888888888888888888888888888
+
+
+//this function is for taking input in the function
 function getInputNumber(id) {
     const amount = document.getElementById(id).value;
-    const amountNumber = parseFloat(amount);
+    const amountNumber = parseFloat(amount); //we are putting value as a string and parseFloat() is converting it in a number.
     return amountNumber;
-
+    // we are returning it bcoz if the rest amount should be shown in the balance . withdraw 500 from 1500 then return na hole 1000 pabo na.
+    // so we have to sore the rest of amount and pass it in the id. 
 
 }
 
